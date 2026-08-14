1531be2af7abd2de9c1f20de1753e7e09e4e20f1
silas_ <silasifmgformiga@gmail.com>
2026-08-14 20:21:49 -0300

    atividade prática 1 iniciada

---
+++ scripts/01-atividade.R
@@ -0,0 +1,16 @@
+# Arquivo: 01-atividade.R
+# Autor: Silas Henrique
+# Data: 14/08/26
+# Objetivos:
+# 1. Executar a atividade prática 1
+
+# Configurações globais ------------------------------------------
+
+# Configura o número de dígitos a serem exibidos
+options(digits = 5, scipen = 999)
+
+# carrega os pacotes necessários
+library(here) # para usar caminhos relativos
+library(tidyverse) # carrega o dplyr, readr, ggplot2, etc.
+
+# Aquisição dos dados --------------------------------------------
